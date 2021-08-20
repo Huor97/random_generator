3b1a3a0a19751f36c93e0ff2b177728ed50abfcf
Huor97 <huor97ka@gmail.com>
2021-08-20 16:53:11 +0200

    aleatoir citation V03

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// ----------------------- function générator aléatoir
+// ----------------------- function générateur aléatoir
 
 // générato aléatoir color
 buttonColor.addEventListener("click", function (e) {
@@ -11,27 +11,29 @@ buttonColor.addEventListener("click", function (e) {
 
   hex.textContent = color;
 });
-// générator aléatoir citation
+// générateur aléatoir citation
 // crée un tableau de citation objet
 // déclar un variable pour stocker objet
 // crée un bocle pour générer aléatoir
 buttonCitation.addEventListener("click", function (e) {
-  // citaionP;
-  // let citationOutput = "Ce n'est pas vrais la realiter";
   monjison = {
-    1: "Ce n'est pas vrais la realiter",
-    2: "Ce n'est pas vrais la realiter2",
-    3: "Ce n'est pas vrais la realiter3",
-    4: "Ce n'est pas vrais la realiter4",
-    5: "Ce n'est pas vrais la realiter5",
-    6: "Ce n'est pas vrais la realiter6",
-    7: "Ce n'est pas vrais la realiter7",
-    8: "Ce n'est pas vrais la realiter8",
+    0: "Le souvenire, c'est la présence invisible: Victor Hugo",
+    1: "Le courage n'est pas l'absence de peur, mais la capacité de vaincre ce qui fait peur: Nelson Mandela",
+    2: "Un problème sans solution est un problème mal posé: Albert Einstein",
+    3: "Deux choses sont infinies: l'Univers et la bêtise humaine. Mais en ce qui concerne l'Univers, je n'en ai pas encore acquis la certitude absolue: Albert Einstein",
+    4: "Quand on veut on peut, quand on peut on doit, Napoléon Bonaparte",
+    5: "C'est de l'enfer des pauvres qu'est fait le paradi des riches: Vicor Hugo",
+    6: "Ce n'est pas vrais la realiter7",
+    7: "Ce n'est pas vrais la realiter8",
   };
+  let monjisonMin = 0;
+  let monjisonMax = 7;
   let citaionResulta;
+  //La fonction Math.floor(x) renvoie le plus grand entier qui est inférieur ou égal à un nombre x
+  citaionResulta =
+    monjison[
+      Math.floor(Math.random() * (monjisonMax - monjisonMin + 1)) + monjisonMin
+    ];
 
-  for (let i = 0; i < 8; i++) {
-    citaionResulta = monjison[Math.floor(Math.random() * 10)];
-  }
   citaionP.textContent = citaionResulta;
 });
